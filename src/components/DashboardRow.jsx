850b47808b7b150435dc662cf8ad58332833db1d
Elijah Hensel <elijah.witth@gmail.com>
2021-10-05 17:23:51 -0500

    removed inline styling and used makeStyles in dashboardRows

--- src/components/DashboardRow.jsx
+++ src/components/DashboardRow.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  idCell: {
+    fontWeight: "bold",
+    color: "white",
+    padding: "5px",
+  },
+  cell: {
+    color: "white",
+    padding: "5px",
+  },
+});
 
 const DashboardRow = ({
   allServers: {
@@ -12,6 +25,7 @@ const DashboardRow = ({
     up_since,
   },
 }) => {
+  const classes = useStyles();
   const dateFromData = new Date(up_since);
   const convDate = dateFromData.toLocaleDateString();
   let convTime = dateFromData.toTimeString();
@@ -25,30 +39,20 @@ const DashboardRow = ({
         tabIndex={-1}
         key={id}
       >
-        <TableCell
-          style={{ fontWeight: "bold", color: "white", padding: "5px" }}
-        >
-          {id ? id : "n/a"}
-        </TableCell>
-        <TableCell style={{ color: "white", padding: "5px" }}>
+        <TableCell className={classes.idCell}>{id ? id : "n/a"}</TableCell>
+        <TableCell className={classes.cell}>
           {allocated_ram ? allocated_ram : "n/a"}
         </TableCell>
-        <TableCell style={{ color: "white", padding: "5px" }}>
+        <TableCell className={classes.cell}>
           {free_ram ? free_ram : "n/a"}
         </TableCell>
-        <TableCell
-          style={{ color: "white", padding: "5px", fontSize: "small" }}
-        >
+        <TableCell className={classes.cell}>
           {allocated_disk ? allocated_disk : "n/a"}
         </TableCell>
-        <TableCell
-          style={{ color: "white", padding: "5px", fontSize: "small" }}
-        >
+        <TableCell className={classes.cell}>
           {free_disk ? free_disk : "n/a"}
         </TableCell>
-        <TableCell
-          style={{ color: "white", padding: "5px", fontSize: "small" }}
-        >
+        <TableCell className={classes.cell}>
           {up_since ? `${convDate} @ ${convTime}` : "n/a"}
         </TableCell>
       </TableRow>
